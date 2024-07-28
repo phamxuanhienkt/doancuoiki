c85e0b38fe368d3a5c4454ad51eed3ae73eaf073
tranhuyendieuit <tranhuyendieuit@gmail.com>
2024-07-28 17:24:49 +0700

    Update models.py và serializers.py

--- apps/app/api/models.py
+++ apps/app/api/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class MyUserManager(BaseUserManager):
-    def  create_user(self, email, password=None):
+    def  create_user(self, email, username, password=None):
         if not email:
             raise ValueError('User must have an email address')
-        user = self.model(email=self.normalize_email(email))
+        if not username:
+            raise ValueError('User must have a username')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -23,6 +28,7 @@ class MyUse(AbstractBaseUser):
     is_superuser = None
     # email = models.EmailField(unique=True)
     email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -31,7 +37,7 @@ class MyUse(AbstractBaseUser):
     objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.email
--- apps/app/api/serializers.py
+++ apps/app/api/serializers.py
@@ -7,12 +7,13 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'password']
+        fields = ['id', 'username','email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data['email'],
+            username=validated_data['username'],
             password=validated_data['password']
         )
         return user
